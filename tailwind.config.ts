9de480059fd1b0d64ee59dc7d5cfc2a27799b7ec
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 17:58:56 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+				sans: ["var(--font-sans)", "sans-serif"],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
